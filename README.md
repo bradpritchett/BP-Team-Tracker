dd752b57cd2be701134205e70df30a78334c50fb
Brad Pritchett <bradpritchett1@msn.com>
2020-07-24 00:13:51 -0600

    Removed console logs and refined output

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ This simple project allows users to generate an organization chart for their com
 Users are prompted via CLI on each team member. After each team member is entered the user is asked if they would like to add another. If they do, then another employee can be added. Otherwise, the team.html file is generated.
 
 ## Watch the video
-A demo video can be viewed here [video](https://drive.google.com/file/d/1oBXdBEIWgOQ9TCkCYtTBZS-9eQHJmr8N/view)
+A demo video can be viewed [here](https://drive.google.com/file/d/1oBXdBEIWgOQ9TCkCYtTBZS-9eQHJmr8N/view)
 ## Installation Instructions
 Requires NODE. Run npm i to install dependencies.
 ## Questions
--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const employees = [];
-
-const util = require("util");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -12,9 +9,8 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-const Employee = require("./lib/Employee");
 
-
+const employees = [];
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
@@ -22,7 +18,7 @@ function promptUser() {
     return inquirer.prompt([{
             type: "input",
             name: "name",
-            message: "New Employee's Name?"
+            message: "What is the new Employee's Name?"
         },
         {
             type: "input",
@@ -87,7 +83,7 @@ function afterPrompt(answers) {
 
     switch (answers.role) {
         case "Manager":
-            const manager = new Manager(answers.name, answers.id, answers.email, answers.OfficeNumber)
+            const manager = new Manager(answers.name, answers.id, answers.email, answers.OfficeNumber);
             employees.push(manager);
             break;
         case "Engineer":
@@ -101,44 +97,13 @@ function afterPrompt(answers) {
     }
 
     if (answers.addMore === "Yes") {
-        console.log(employees)
-        console.log("adding another!")
-        promptUser().then(afterPrompt)
+        console.log("Adding another!")
+        promptUser().then(afterPrompt);
     } else {
-        console.log(employees)
-        console.log("Finished adding!")
-
-
-        // After the user has input all employees desired, call the `render` function (required
-        // above) and pass in an array containing all employee objects; the `render` function will
-        // generate and return a block of HTML including templated divs for each employee!
-
-
+        console.log("HTML Generated!")
         var html = render(employees);
-
-        console.log(html)
     }
     fs.writeFileSync("output/team.html", html);
-}
+};
 
 promptUser().then(afterPrompt);
-
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
-
-//return 
